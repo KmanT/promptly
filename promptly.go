4b1073499d0359f6bf1fd15599c9e8c903a15b3e
Kyle Turske <kyle.turske@merkle.com>
2024-01-26 16:41:08 -0500

    feat(promptly.go): add bounds checking and errors

--- promptly.go
+++ promptly.go
@@ -2,9 +2,11 @@ package promptly
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 
 	//"os"
+	"math"
 	"regexp"
 	"strconv"
 	"strings"
@@ -16,25 +18,42 @@ func GetSimplePromptText(rdr *bufio.Reader, prmpt string) string {
 	return strings.TrimRight(txt, "\n")
 }
 
-func GetTypeAndBits(typeName string) (string, int) {
-	if !regexp.MustCompile(`[a-z]+\d+`).MatchString(typeName) {
-		return typeName, 0
+func GetTypeAndBits(typeName string) (string, int, error) {
+	if !regexp.MustCompile(`[a-z]+\d{1,3}`).MatchString(typeName) {
+		return typeName, 0, nil
 	}
 
 	name := regexp.MustCompile(`[a-z]+`).FindString(typeName)
-	bitStr := regexp.MustCompile(`\d+`).FindString(typeName)
+	bitStr := regexp.MustCompile(`\d{1,3}`).FindString(typeName)
 
 	bits, err := strconv.Atoi(bitStr)
 	if err != nil {
 		fmt.Printf("Warning: could not find bits in %q", typeName)
-		bits = 0
+		return name, 0, nil
 	}
 
-	return name, bits
+	if bits < 8 {
+		fmt.Print("Error: the number of bits must be greater than 8.")
+		return name, 0, errors.New("BitOutOfRangeError")
+	}
+
+	if bits >= 128 {
+		fmt.Print("Error: the number of bits must be less than or equal to 128.")
+		return name, 0, errors.New("BitOutOfRangeError")
+	}
+
+	boL2 := math.Log2(float64(bits))
+
+	if math.Mod(boL2, 1.0) != 0.0 {
+		fmt.Print("Error: the number of bits must be a power of 2.")
+		return name, 0, errors.New("BitNotPowerOfTwoError")
+	}
+
+	return name, bits, nil
 }
 
 func ConvertInputToType(input *string, t string) (interface{}, error) {
-	name, bits := GetTypeAndBits(t)
+	name, bits, _ := GetTypeAndBits(t)
 
 	switch name {
 	case "int":
@@ -53,3 +72,5 @@ func ConvertInputToType(input *string, t string) (interface{}, error) {
 		return *input, nil
 	}
 }
+
+// TODO: prompt verification
