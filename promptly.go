23a9ca83f299833e5d2a5bc49c6c8c64c2f9e795
Kyle Turske <kyle.turske@merkle.com>
2024-01-30 10:56:09 -0500

    docs(promptly.go): go docs

--- promptly.go
+++ promptly.go
@@ -1,3 +1,7 @@
+// Package promptly has helper functions to make CLI Application building easy.
+//
+// The promptly package should only be used for CLI applications that require
+// terminal interaction.
 package promptly
 
 import (
@@ -7,12 +11,17 @@ import (
 	"strings"
 )
 
+// GetSimplePromptText gets a single line from the bufio Reader.
+// It also removes the line break '\n' from the input.
 func GetSimplePromptText(rdr *bufio.Reader, prmpt string) string {
 	fmt.Println(prmpt)
 	txt, _ := rdr.ReadString('\n')
 	return strings.TrimRight(txt, "\n")
 }
 
+// GetPromptVerify gets a single line from the bufio Reader, and checks if the
+// input is included in the list of valid input. GetPromptVerify returns a
+// boolean based on its validity and the input received.
 func GetPromptVerify(rdr *bufio.Reader, prmpt string, vi []string, caseS bool) (bool, string) {
 	in := GetSimplePromptText(rdr, prmpt)
 
@@ -28,6 +37,9 @@ func GetPromptVerify(rdr *bufio.Reader, prmpt string, vi []string, caseS bool) (
 
 // TODO: Verify with Regex func
 
+// GetPromptVerifyLoop attempts to get input until the input is valid. If the
+// input received is invalid, GetPromptVerify will loop again and ask for input
+// again. Once the input is valid GetPromptVerify will return the user's choice.
 func GetPromptVerifyLoop(rdr *bufio.Reader, prmpt string, vi []string, caseS bool) string {
 	var in string
 	isValid := false
@@ -41,13 +53,11 @@ func GetPromptVerifyLoop(rdr *bufio.Reader, prmpt string, vi []string, caseS boo
 	for !isValid {
 		// isValid, in = GetPromptVerify(rdr, prmpt, vi, caseS)
 		in = GetSimplePromptText(rdr, prmpt)
-
 		if !caseS {
 			in = strings.ToLower(in)
 		}
 
 		isValid = vIM[in]
-
 		if !isValid {
 			fmt.Printf("Input '%s' is invalid. Try again", in)
 		}
@@ -55,6 +65,9 @@ func GetPromptVerifyLoop(rdr *bufio.Reader, prmpt string, vi []string, caseS boo
 	return in
 }
 
+// sliceToBoolMap is helper function that takes in a slice of types that
+// implement cmp.Ordered (as O) and returns a map with the O type as a key
+// and bool as the value. Effectively creating a set
 func sliceToBoolMap[O cmp.Ordered](slice []O) map[O]bool {
 	m := make(map[O]bool)
 
@@ -65,6 +78,8 @@ func sliceToBoolMap[O cmp.Ordered](slice []O) map[O]bool {
 	return m
 }
 
+// stringSliceToLower is a helper function that mutates a string slice to
+// lowercase.
 func stringSliceToLower(slice *[]string) {
 	for i, el := range *slice {
 		(*slice)[i] = strings.ToLower(el)
